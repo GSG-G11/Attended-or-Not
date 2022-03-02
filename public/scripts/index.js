38ab63fac658dfa79d7c6c1786e510d718aa34ed
mayar-kabaja <kabajamayar@gmail.com>
2022-03-02 09:06:06 +0200

    create admin page Relates #18

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,9 @@
 import request from './request.js';
 const addUserBtn = document.querySelector('.add-user-btn');
 const name = document.querySelector('.name-input');
-const program = document.querySelector('.program-input');
 const phone = document.querySelector('.phone-input');
 const date = document.querySelector('.date-input');
+const program = document.querySelector('#Program');
 
 addUserBtn.addEventListener('click', () => {
   const user = {
