484193704958365a6183723d995698e414ddd6ac
mayar-kabaja <kabajamayar@gmail.com>
2022-03-02 09:59:59 +0200

    write to program Relates #20

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,8 @@ const getDataFromDB = require('../database/queries/getData');
 const router = express.Router();
 
 const getDataFromPublic = require('../controllers/getData');
-
+const path =  require('path')
 router.post('/add_user', getDataFromPublic);
 
+router.post('/add_program' , getDataFromPublic)
 module.exports = router;
