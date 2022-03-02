d6395aca73b2f66c439b966b8ed292b1ac9a6724
mayar-kabaja <kabajamayar@gmail.com>
2022-03-02 17:54:04 +0200

    error handle Relates #33

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,20 @@
-const path = require('path');
+const {join} = require('path');
 const express = require('express');
 const getPrograms = require('../controllers/getPrograms');
 const router = express.Router();
-
 const {getDataFromProgram ,getDataFromUser} = require('../controllers/getData');
+const { notFoundError, serverError } = require('../controllers/error');
+
+router.use(express.static(join(__dirname, '..','..', 'public')));
 
 router.post('/add_user', getDataFromUser);
 
 router.post('/add_program',getDataFromProgram );
 
 router.get('/get_program', getPrograms);
+
+router.use(notFoundError);
+
+router.use(serverError);
 
 module.exports = router;
