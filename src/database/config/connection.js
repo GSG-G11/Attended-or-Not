f44a08950ae2fc37a2629143c724192fd957724f
Amjad Yahia <amjedyehia1998@gmail.com>
2022-03-02 18:51:00 +0200

    tst

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -10,7 +10,8 @@ if (process.env.NODE_ENV === 'production') dbURL = process.env.DATABASE_URL;
 if (!dbURL) throw new Error('no db url');
 
 const option = {
-  connectionString: dbURL,
+  connectionString:
+    'postgres://vcnoaqwlwlassg:c377c431d23e915f7fff54ea8f17f973133feea2870e96118a644a49f59d446c@ec2-35-153-35-94.compute-1.amazonaws.com:5432/d8b9gum3flpf9t',
   ssl: {
     rejectUnauthorized: false,
   },
